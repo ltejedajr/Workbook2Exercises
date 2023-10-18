a803a98ba2f8d0f0449d57bfe452dc10ec096d82
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-17 20:24:12 -0500

    Modified Mathscripts folder and its scripts

--- MathScripts/area_of_circle.js
+++ MathScripts/area_of_circle.js
@@ -5,5 +5,5 @@ var pi = 3.14;
 // calculations
 var area = 3.14 * Math.pow(radius,5);
 
-// outputs
+// output
 console.log("The area of a circle with radius of 5 is " + area);
--- MathScripts/rule_of_72.js
+++ MathScripts/rule_of_72.js
@@ -4,5 +4,5 @@ var interestRate = 5;
 // calculations
 var yearsToDouble =  72 / interestRate;
 
-// outputs
-console.log("At a 5% interest rate, your savings account will be worth $13,000 in " + yearsToDouble + " years");
+// output
+console.log("At a 5% interest rate, your savings account will be worth $19,000 in " + yearsToDouble + " years");
