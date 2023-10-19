f239e31b354b72e484392c473a066ff36b6430ef
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-19 09:07:28 -0500

    Added new script

---
+++ NumericConversions/conversion_tests.js
@@ -0,0 +1,54 @@
+"use strict";
+
+// Description: This script tests various numeric 
+//              conversion techniques
+// Author: Lawrence T. Newprogrammer
+
+// var a = " 101.1 ";
+// var b = "55";
+// var c = "402 Stevens";
+// var d = "Number 5 ";
+
+// parseInt
+
+// var a = parseInt (" 101.1 ");
+// var b = parseInt ("55");
+// var c = parseInt ("402 Stevens");
+// var d = parseInt ("Number 5 ");
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// parseFloat
+
+// var a = parseFloat (" 101.1 ");
+// var b = parseFloat ("55");
+// var c = parseFloat ("402 Stevens");
+// var d = parseFloat ("Number 5 ");
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// var a = Number (" 101.1 ");
+// var b = Number ("55");
+// var c = Number ("402 Stevens");
+// var d = Number ("Number 5 ");
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+var a =  + (" 101.1 ");
+var b =  + ("55");
+var c =  + ("402 Stevens");
+var d =  + ("Number 5 ");
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
