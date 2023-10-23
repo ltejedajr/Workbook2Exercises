f53ec8c07ee9271182350a32988140d0a04cf120
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-23 13:09:09 -0500

    Modified min_max.js

--- IfScripts/min_max.js
+++ IfScripts/min_max.js
@@ -4,3 +4,14 @@ let a = 100;
 let b = 200;
 let c = 300;
 
+if (a > b && a > c ) {
+    "A = "+ a +" is the greatest"
+  }
+  
+  else if (c > a && c > b) {
+     "B = "+ b +" is the greatest"
+  }
+      
+  else "C = " + c +" is the greatest"
+  
+  console.log();
