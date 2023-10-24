ca581bc0e181fadcd9c8fda3c8a9bdad66cd9147
ltejedajr <126844440+ltejedajr@users.noreply.github.com>
2023-10-24 13:05:53 -0500

    Modified f_to_c.js

--- MathScripts/f_to_c.js
+++ MathScripts/f_to_c.js
@@ -1,10 +1,19 @@
-"use strict";
+// "use strict";
 
-// input
-var fahrenheitTemperature = 75;
+// // input
+// var fahrenheitTemperature = 75
 
-// calculations
-var celsiusTemperature =  (fahrenheitTemperature - 32) * 5/9;
+// // calculations
+// var celsiusTemperature =  (fahrenheitTemperature - 32) * 5/9;
 
-// output
-console.log(fahrenheitTemperature + " degrees Fahrenheit is equal to " + celsiusTemperature.toFixed(2) + " degrees Celsius. ");
+// // output
+// console.log(fahrenheitTemperature + " degrees Fahrenheit is equal to " + celsiusTemperature.toFixed(2) + " degrees Celsius. ");
+
+function convertFtoC(fahrenheitTemperature) {
+    let celsiusTemperature =  (fahrenheitTemperature - 32) * 5/9;
+    return celsiusTemperature;
+}
+
+    let fahrenheitTemperature = 92;
+    let celsiusTemperature = convertFtoC(fahrenheitTemperature);
+    console.log(fahrenheitTemperature + " degrees Fahrenheit is " + celsiusTemperature.toFixed(2) + " degress Celsius");
